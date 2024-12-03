ceca71520e2356831fc6c01108d850738877acb5
Taifun2005 <lider2005@yandex.ru>
2024-12-03 20:59:46 +0700

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,23 @@
+import time
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(1,6):
+        await asyncio.sleep(power)
+        print(f'Силач {name} поднял {i}')
+    print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('djon',5))
+    task2 = asyncio.create_task(start_strongman('marat',7))
+    task3 = asyncio.create_task(start_strongman('djini',3))
+    await task1
+    await task2
+    await task3
+
+start = time.time()
+asyncio.run(start_tournament())
+end = time.time()
+
+print(f'Финиш! все силачи выпольнили задание за  {end - start} время')
